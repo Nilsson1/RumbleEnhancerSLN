109349e419c4fd8a6ea9b34e8a97fde4ff0d4311
Nilsson1 <userfrenils@gmail.com>
2023-02-09 21:52:19 +0100

    Add project files.

---
+++ RumbleEnhancerWebSite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RumbleEnhancerWebSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
